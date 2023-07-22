558541ad74aea95feabe9038d1471568bc0cd251
jafar900 <jafarmohammed900@gmail.com>
2023-07-22 13:05:44 +0530

    FindLargestNumber

---
+++ FindLargestNumber.js
@@ -0,0 +1,16 @@
+// Find the largest number among three numbers
+
+
+var a=155;
+var b=12;
+var c=13;
+
+if(a>b && a>c){
+    console.log(`${a} is the largest number`)
+}
+else if(b>a && b>c){
+    console.log(`${b} is the largest number`)
+}
+else{
+    console.log(`${c} is the largest number`)
+}
