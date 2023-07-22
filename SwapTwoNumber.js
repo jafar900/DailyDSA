ec20c94912c38763ae9dd731f7e12243bcf70f0b
jafar900 <jafarmohammed900@gmail.com>
2023-07-22 12:58:50 +0530

    swapnumber

---
+++ SwapTwoNumber.js
@@ -0,0 +1,13 @@
+var a=10;  // a=12 b=10 c=11
+var b=11;
+var c;
+
+
+ c=a;  // c value is 10
+ a=b;   //a value is 11
+ b=c; // b value is 10
+
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
+console.log(`The value of b after swapping: ${c}`);
