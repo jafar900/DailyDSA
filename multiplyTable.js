357719814d9b1219345e4a4e5cfbfa5d687018fb
jafar900 <jafarmohammed900@gmail.com>
2023-08-03 15:31:57 +0530

    Tables

---
+++ multiplyTable.js
@@ -0,0 +1,15 @@
+// function multiplyTable(n){
+   var n=3;
+    if(n>=1){
+        for(i=1;i<=10;i++){
+            var result=i*n;
+          console.log(`${n}*${i}=${result}`)
+        }
+    }else{
+        console.log(`enter positive integer`)
+    }
+  // return 
+   
+//}
+
+// console.log(multiplyTable(-3))
